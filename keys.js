2576a1ab825f4d006380267768844112d4bf4a9f
Bradley Gamboa <bradleygamboa@gmail.com>
2016-10-10 18:12:39 -0500

    added dependencies

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'K248nlYZUpOgjcSRedaxSBIhr',
+  consumer_secret: 'W3EBfYXIZfVYUmtCpyqIDa9ApEVuEUFHjOwlNjVJJugQsceIE0',
+  access_token_key: '16656979-dwcCPM28dC3NJEtH0O09EL7QEBdWLj56oKPXxOSsk',
+  access_token_secret: 'bquQq6DVfj3VPW2cHBbjV53nOOlxFrkOPStxe9eiO6rgl',
+}
